4e71cb29eb235c13ecf7f7d133ae4009c9f767db
kriszkk03 <44733842+kriszkk03@users.noreply.github.com>
2023-04-08 11:25:35 +0200

    test from terminal

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Init!</Text>
+      <Text>test commit!</Text>
 
     </View>
   );
